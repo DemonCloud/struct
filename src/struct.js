cbc92377bca4f46f426cea7fb722c706731ffc82
YiJun <cloudywolf1@qq.com>
2017-02-08 22:12:16 +0800

    add struct.js

---
+++ src/struct.js
@@ -0,0 +1,13 @@
+/* 
+ * Struct
+ *
+ * slim Javascript untils lib
+ * C lang & trunk with K&R <The C programming language>
+ * support web browser and node.js
+ *
+ * @Author  : YiJun
+ * @Date    : 2017.2.08 - now
+ * @Version : 0.1
+ *
+ * @License : Fuck any LISCENSE
+ */
